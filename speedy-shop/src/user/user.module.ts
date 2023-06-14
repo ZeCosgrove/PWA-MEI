0c84d06dabd7458b4d1248f9550200cb71f45be2
José Cosgrove <zecosgrove@gmail.com>
2023-06-14 20:53:08 +0100

    JWT and AuthGuards and Roles

--- speedy-shop/src/user/user.module.ts
+++ speedy-shop/src/user/user.module.ts
@@ -2,14 +2,28 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
 
 import {User, UserSchema} from './schemas/user.schema';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './auth/roles.guard';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [
+    UserService,
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    }
+  ],
   imports: [MongooseModule.forFeature([
     { name: User.name, schema: UserSchema }
-  ])]
+  ]), 
+  JwtModule.register({
+    global: true,
+    secret: "asasd", //process.env.SECRET,
+    signOptions: { expiresIn: '3600s' },
+  })]
 })
 export class UserModule {}
